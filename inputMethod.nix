424f12a0c82a895948af84ad12c55e679dbe611e
HeavenTao <542248377@qq.com>
2025-03-03 14:21:20 +0000

    1.inputMethod ok

--- inputMethod.nix
+++ inputMethod.nix
@@ -13,15 +13,26 @@
   ];
 in {
   users.users.${userName}.packages = packages;
-  services.xserver.desktopManager.runXdgAutostartIfNone = false;
+  environment.variables = {
+    GLFW_IM_MODULE = "fcitx";
+    GTK_IM_MODULE = "fctix";
+    INPUT_METHOD = "fcitx";
+    XMODIFIERS = "@im=fcitx";
+    IMSETTINGS_MODULE = "fcitx";
+    QT_IM_MODULE = "fcitx";
+    SDL_IM_MODULE = "fctix";
+  };
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
     fcitx5 = {
       waylandFrontend = true;
       addons = with pkgs; [
+        rime-zhwiki
+        rime-data
+        fcitx5-rime
+        librime
         fcitx5-chinese-addons
-        fcitx5-gtk
         fcitx5-configtool
       ];
     };
