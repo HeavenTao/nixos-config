166f482c57566099806c8291a8f9fd40675b9803
HeavenTao <542248377@qq.com>
2025-03-06 20:40:40 +0800

    1.add rofi settings

--- home.nix
+++ home.nix
@@ -13,6 +13,15 @@
   home.file.".tmux.conf" = {
     source = ./config/tmux/.tmux.conf;
   };
+  home.file.".config/ghostty/config" = {
+    source = ./config/ghostty/config;
+  };
+  home.file.".config/rofi/config.rasi" = {
+    source = ./config/rofi/config.rasi;
+  };
+  home.file.".config/rofi/gruvbox-dark-hard.rasi" = {
+    source = ./config/rofi/gruvbox-dark-hard.rasi;
+  };
   home.stateVersion = "24.11";
   programs.home-manager.enable = true;
 }
