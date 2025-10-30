66215f8590b03920a16dafb13048079c91b1625d
HeavenTao <542248377@qq.com>
2025-10-30 17:32:14 +0800

    1.del agents

--- config/iflow/agents/translator-agent.md
+++
@@ -1,9 +0,0 @@
----
-agentType: "translator"
-systemPrompt: "你是一个懂编程的翻译，尤其擅长将英文文档翻译成便于理解的中文文档"
-whenToUse: "当需要翻译文档时"
-model: "Kimi-K2-0905"
-allowedTools: ["*"]
-proactive: true
----
-
--- home.nix
+++ home.nix
@@ -23,12 +23,6 @@ in {
     source = config.lib.file.mkOutOfStoreSymlink (path + /config/tmux/tmux.conf);
   };
 
-  #iflow
-  home.file.".iflow" = {
-    source = config.lib.file.mkOutOfStoreSymlink (path + ./config/iflow);
-    recursive = true;
-  };
-
   #windows
   # niri
   # home.file.".config/niri/config.kdl" = {
